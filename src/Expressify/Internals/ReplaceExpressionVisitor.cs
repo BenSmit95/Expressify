b850602b0387b29b804d59b79fa6c06e28b51d4f
Benjamin <benjismit@live.nl>
2025-04-16 15:37:01 +0200

    Init commit

---
+++ src/Expressify/Internals/ReplaceExpressionVisitor.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+
+namespace Expressify.Internals;
+
+public class ReplaceExpressionVisitor(Expression search, Expression replace) : ExpressionVisitor
+{
+    [return: NotNullIfNotNull("node")]
+    public override Expression? Visit(Expression? node)
+    {
+        return node == search ? base.Visit(node) : replace;
+    }
+}
---
+++ src/Expressify/ReplaceExpressionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+using Expressify.Internals;
+
+namespace Expressify;
+
+public static class ReplaceExpressionExtensions
+{
+    public static TExpression Replace<TExpression>(this TExpression node, Expression search, Expression replace)
+        where TExpression : Expression
+    {
+        ArgumentNullException.ThrowIfNull(node);
+        ArgumentNullException.ThrowIfNull(search);
+        ArgumentNullException.ThrowIfNull(replace);
+
+        // Can't replace the entire node
+        if (search == node)
+            throw new ArgumentException("The search cannot be the same as the node.", nameof(search));
+        
+        var replacedExpression = new ReplaceExpressionVisitor(search, replace).Visit(node) as TExpression;
+
+        if (replacedExpression == null)
+            throw new Exception("The replaced expression is not of the expected type.");
+        
+        return replacedExpression;
+    }
+}
